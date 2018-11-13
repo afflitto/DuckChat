018f2b221878f1d06bccdd1ba0fa04a457aa29d7
LegoMaster616 <andrew.afflitto@gmail.com>
2018-11-13 00:28:24 -0500

    add keypair generator

---
+++ src/DuckyKeyPair.java
@@ -0,0 +1,43 @@
+import java.security.*;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class DuckyKeyPair {
+	
+	private KeyPair pair;
+	
+	public DuckyKeyPair() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException {
+		new DuckyKeyPair(1024);
+	}
+	
+	public DuckyKeyPair(int keySize) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+		KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+		keygen.initialize(keySize);
+				
+		pair = keygen.generateKeyPair();
+	}
+	
+	public DuckyKeyPair(KeyPair pair) {
+		this.pair = pair;
+	}
+	
+	public String encrypt(String msg) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
+		return Base64.getEncoder().encodeToString(cipher.doFinal(msg.getBytes()));
+		
+	}
+	
+	public String decrypt(String msg) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.DECRYPT_MODE, pair.getPublic());
+		byte[] cipherText = Base64.getDecoder().decode(msg);
+		return new String(cipher.doFinal(cipherText));
+	}
+	
+	
+}
