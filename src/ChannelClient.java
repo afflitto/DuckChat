dc144a3817e50998fd5132dc6a7bcc332e29de6a
Matt Duffy <duffymatt3214@yahoo.com>
2018-12-03 15:19:19 -0500

    Added echo server stuff

--- src/ChannelClient.java
+++ src/ChannelClient.java
@@ -27,93 +27,86 @@ import com.duckchat.protocol.DebugMessage;
  * capitalizing them.````````
  */
 
-public class ChannelClient implements Runnable{
+public class ChannelClient implements Runnable {
 
-    private ServerConnection connection;
-    private ChannelManager manager;
-    
-    private String username = "";
-    private String channel = "";
+	private ServerConnection connection;
+	private ChannelManager manager;
 
-    
-    private JFrame frame = new JFrame("Channel Client");
-    private JMenuBar menuBar = new JMenuBar();
-    private JMenu menu = new JMenu("Admin");
-    private JMenuItem keyGenItem = new JMenuItem("Generate new Group Key");
-    private JMenuItem closeServerItem = new JMenuItem("Close Server");
-    private JMenuItem listUsersItem = new JMenuItem("List users");
-    private JTextField dataField = new JTextField(40);
-    private JTextArea messageArea = new JTextArea(60, 120);
-    
+	private String username = "";
+	private String channel = "";
 
+	private JFrame frame = new JFrame("Channel Client");
+	private JMenuBar menuBar = new JMenuBar();
+	private JMenu menu = new JMenu("Admin");
+	private JMenuItem keyGenItem = new JMenuItem("Generate new Group Key");
+	private JMenuItem closeServerItem = new JMenuItem("Close Server");
+	private JMenuItem listUsersItem = new JMenuItem("List users");
+	private JTextField dataField = new JTextField(40);
+	private JTextArea messageArea = new JTextArea(60, 120);
 
-    /**
-     * Constructs the client by laying out the GUI and registering a
-     * listener with the textfield so that pressing Enter in the
-     * listener sends the textfield contents to the server.
-     */
-    public ChannelClient() {
+	/**
+	 * Constructs the client by laying out the GUI and registering a listener with
+	 * the textfield so that pressing Enter in the listener sends the textfield
+	 * contents to the server.
+	 */
+	public ChannelClient() {
 //gather user info
-    	username = JOptionPane.showInputDialog("What is your name?");
-    	channel = JOptionPane.showInputDialog("What Channel would you like to join?");
-    	
-    	
-        // Layout GUI
-        messageArea.setEditable(false);
-        frame.getContentPane().add(dataField, "South");
-        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
-        
-        menuBar.add(menu);
-        menu.add(keyGenItem);
-        menu.add(closeServerItem);
-        menu.add(listUsersItem);
-        frame.setJMenuBar(menuBar);
-        
-        manager = new ChannelManager(messageArea);
-        
-        keyGenItem.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		DuckySymmetricKey newKey = new DuckySymmetricKey();
+		username = JOptionPane.showInputDialog("What is your name?");
+		channel = JOptionPane.showInputDialog("What Channel would you like to join?");
+
+		// Layout GUI
+		messageArea.setEditable(false);
+		frame.getContentPane().add(dataField, "South");
+		frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+
+		menuBar.add(menu);
+		menu.add(keyGenItem);
+		menu.add(closeServerItem);
+		menu.add(listUsersItem);
+		frame.setJMenuBar(menuBar);
+
+		manager = new ChannelManager(messageArea);
+
+		keyGenItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DuckySymmetricKey newKey = new DuckySymmetricKey();
 				System.out.println("new sym key: " + newKey.encodeKey());
-				for(User user : Application.users) {
+				for (User user : Application.users) {
 					NewKeyMessage message = new NewKeyMessage(channel, newKey, user.getPubKey());
 					connection.send(message);
 				}
-        		//connection.send(new NewKeyMessage(channel, newKey, new DuckyPublicKey(manager.getPair().getPublicKey())));
-        	}
-        });
-        closeServerItem.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		connection.send(new DebugMessage(0,channel,manager.getSymmetricKey()));
-        	}
-        });
-        listUsersItem.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		System.out.println("Users:");
-        		messageArea.append("Users:");
-        		for(User user : Application.users) {
-        			System.out.println("\t"+user.getName()+":"+user.getPubKey().encode());
-            		messageArea.append("\t"+user.getName()+":"+user.getPubKey().encode());
-        		}
-        		System.out.println();
-        		messageArea.append("\n");
-        	}
-        });
+				// connection.send(new NewKeyMessage(channel, newKey, new
+				// DuckyPublicKey(manager.getPair().getPublicKey())));
+			}
+		});
+		closeServerItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				connection.send(new DebugMessage(0, channel, manager.getSymmetricKey()));
+			}
+		});
+		listUsersItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Users:");
+				messageArea.append("Users:");
+				for (User user : Application.users) {
+					System.out.println("\t" + user.getName() + ":" + user.getPubKey().encode());
+					messageArea.append("\t" + user.getName() + ":" + user.getPubKey().encode());
+				}
+				System.out.println();
+				messageArea.append("\n");
+			}
+		});
 
-        // Add Listeners
-        dataField.addActionListener(new ActionListener() {
-            /**
-             * Responds to pressing the enter key in the textfield
-             * by sending the contents of the text field to the
-             * server and displaying the response from the server
-             * in the text area.  If the response is "." we exit
-             * the whole application, which closes all sockets,
-             * streams and windows.
-             */
-        	
-        	
-        	
-            public void actionPerformed(ActionEvent e) {
+		// Add Listeners
+		dataField.addActionListener(new ActionListener() {
+			/**
+			 * Responds to pressing the enter key in the textfield by sending the contents
+			 * of the text field to the server and displaying the response from the server
+			 * in the text area. If the response is "." we exit the whole application, which
+			 * closes all sockets, streams and windows.
+			 */
+
+			public void actionPerformed(ActionEvent e) {
 				try {
 					connection.send(new TextMessage(username, channel, dataField.getText(), manager.getSymmetricKey()));
 					dataField.setText("");
@@ -136,14 +129,14 @@ public class ChannelClient implements Runnable{
 		// InetAddress serverAddress = InetAddress.getLocalHost();
 		// String addr = JOptionPane.showInputDialog(frame, "address", "duckchat",
 		// JOptionPane.QUESTION_MESSAGE);
-		connection = new ServerConnection("35.196.228.4", 2003);
-		//35.196.228.4
+		connection = new ServerConnection("192.168.1.251", 2003);
+		// connection = new ServerConnection("35.196.228.4", 2003);
+		// 35.196.228.4
 
 		try {
 			manager.setDuckyKeyPair(new DuckyKeyPair(1024));
 			manager.setSymmetricKey(new DuckySymmetricKey());
-			connection.send(new JoinChannelMessage(username, channel,
-					manager.getPair().getPublicKey()));
+			connection.send(new JoinChannelMessage(username, channel, manager.getPair().getPublicKey()));
 		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
